bd016775322e8f38648d43afe2444523a50ecd0d
Umesh-Bhati <umeshbhati193@gmail.com>
2024-05-17 19:34:31 +0530

    update favicon icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -10,15 +10,29 @@ export default function Footer() {
     <div className="w-screen px-[30px] flex border-t border-t-muted/70 self-end justify-between items-center py-[30px]   text-sm text-foreground font-medium  ">
       <span className="">{new Date().getFullYear()}©harshwal.com</span>
       <div className="flex gap-3">
-        <Link href={"#"} className="hover:text-[rgb(53,149,246)]">
+        <a
+          href={"https://www.harshwal.com/"}
+          target="_blank"
+          className="hover:text-[rgb(53,149,246)]"
+        >
           About Harshwal
-        </Link>
-        <Link href={"#"} className="hover:text-[rgb(53,149,246)]">
+        </a>
+        <a
+          href={"https://www.harshwal.com/our-team"}
+          target="_blank"
+          className="hover:text-[rgb(53,149,246)]"
+        >
           Team
-        </Link>
-        <Link href={"#"} className="hover:text-[rgb(53,149,246)]">
+        </a>
+        <a
+          href={
+            "https://api.whatsapp.com/send/?phone=%2B18582990083&text&type=phone_number&app_absent=0"
+          }
+          target="_blank"
+          className="hover:text-[rgb(53,149,246)]"
+        >
           Support
-        </Link>
+        </a>
       </div>
     </div>
   );
